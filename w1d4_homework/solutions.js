89d0e93d15df9a8eeeb7a592edafa5b4cd7bbe59
smithalex24 <smith25243@gmail.com>
2018-03-22 20:34:32 -0700

    Commit 1 - Verbal questions

---
+++ w1d4_homework/solutions.js
@@ -0,0 +1,8 @@
+//FUNCTIONS
+
+//1. Argument is the input and the parameter is how the input is represented in the function.
+//2. Return will stop the function, whereas console.log will print something on the console. Outside of a function, return will shoot back whatever number that it's told.
+
+//PALINDROME
+
+//Write a function checkPalindrome that accepts a single argument, a string. The function should return true (Boolean) if the string is a palindrome, false if it is not. Make sure your function will give the correct answer for words with capital letters.
